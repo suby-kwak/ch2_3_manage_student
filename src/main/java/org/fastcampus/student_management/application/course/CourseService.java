a3b1003a2c82236cb431e0ed7cef80a520da761c
suby_kwak <rhkdbtj@gmail.com>
2024-12-04 16:36:54 +0900

    Update: ToDo Usecase

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -1,10 +1,11 @@
 package org.fastcampus.student_management.application.course;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.CourseList;
 import org.fastcampus.student_management.domain.DayOfWeek;
 import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
@@ -26,10 +27,17 @@ public class CourseService {
 
   public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
     // TODO: 과제 구현 부분
-    return new ArrayList<>();
+    List<Course> courseDayOfWeek = courseRepository.getCourseDayOfWeek(dayOfWeek);
+
+    // 요일에 해당하는 수업들을 반환해야함. 단, 학생 상태가 비활성화 상태이면 수업을 반환해서는 안됨
+    return courseDayOfWeek.stream()
+            .map(CourseInfoDto::new)
+            .collect(Collectors.toList());
   }
 
   public void changeFee(String studentName, int fee) {
     // TODO: 과제 구현 부분
+    CourseList courseList = new CourseList(courseRepository.getCourseListByStudent(studentName));
+    courseList.changeAllCoursesFee(fee);
   }
 }
--- src/main/java/org/fastcampus/student_management/domain/Student.java
+++ src/main/java/org/fastcampus/student_management/domain/Student.java
@@ -7,6 +7,7 @@ public class Student {
   private final String address;
   private boolean activated;
 
+
   public Student(String name, int age, String address) {
     if (name == null || name.isEmpty()) {
       throw new IllegalArgumentException("이름은 필수 입력값입니다.");
@@ -29,7 +30,24 @@ public class Student {
   public String getAddress() {
     return address;
   }
+
   public boolean isActivate() {
     return activated;
+  }
+
+  public void activate() {
+    if (this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    this.activated = true;
+  }
+
+  public void deactivate() {
+    if (!this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    this.activated = false;
   }
 }
